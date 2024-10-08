e4ed30bc57caf3ce5636d347c16cb179a6a45ed1
13923 <Christopher.alessio@vtsd.com>
2024-10-08 13:46:33 -0400

    Updates

--- 1-1-7/Step9.py
+++ 1-1-7/Step9.py
@@ -12,24 +12,26 @@ turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
 for s in turtle_shapes:
   t = trtl.Turtle(shape=s)
   t.color(turtle_colors.pop())
-  t.penup()
   my_turtles.append(t)
-
+  t.penup()
 #
 startx = 0
 starty = 0
-
+direction = 90
 #
+
+
+
 for t in my_turtles:
   t.goto(startx, starty)
+  t.setheading(direction)
   t.pendown
   t.right(45)
   t.forward(50)
-
+  direction = t.heading()
+  startx = t.xcor()
+  starty = t.ycor()
 #
-
-  startx = startx + 50
-  starty = starty + 50
 
 wn = trtl.Screen()
 wn.mainloop()
